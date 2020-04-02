1bfb6eec6fa3e68d46de77f7c7bc90152c8a71ed
Wesley Webster <wesleywebster91@gmail.com>
2020-04-02 18:54:16 -0500

    positioning
    Positioning a few sorta hard going from figma without specs. eyeballing it

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import classes from './Main.module.scss';
-import { Link } from 'react-router-dom';
+import { NavLink, Switch } from 'react-router-dom';
 
 
-const { layout, userUI, terminal } = classes;
+const { layout, userUI, terminal, activeLink, enterBtn } = classes;
 
 const Main = (props) => {
     return (
         <div className={layout}>
             <section className={userUI}>
                 <div className={terminal}>
-                    <Link>log in</Link>
-                    <Link>sign up</Link>
+                    <NavLink exact to="/" activeClassName={activeLink}>log in</NavLink>
+                    <NavLink to="/signUp" activeClassName={activeLink}>sign up</NavLink>
                 </div>
-
+                <button className={enterBtn}>enter</button>
             </section>
-            <div>I am a component</div>
+            <p style={{marginTop: "32em"}}>Rally The Kingdom with your friends.</p>
         </div>
     );
 };
