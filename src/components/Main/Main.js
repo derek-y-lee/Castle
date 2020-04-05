ca89a9aa72b9d36ab1fa0b119a7c736a3ba1a85e
Derek <hdvvlee@gmail.com>
2020-04-04 17:06:52 -0700

    remove {useState} because not used

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+import React from 'react';
 import classes from './Main.module.scss';
 import { NavLink, Link } from 'react-router-dom';
 
