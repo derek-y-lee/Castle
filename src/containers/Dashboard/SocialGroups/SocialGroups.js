5945789777832aa8c36a06e154bbd0a21427aaa9
Wesley Webster <wesleywebster91@gmail.com>
2020-04-08 23:35:02 -0500

    Button fixed
    Should work minus mobile at 300ish pixels

--- src/containers/Dashboard/SocialGroups/SocialGroups.js
+++ src/containers/Dashboard/SocialGroups/SocialGroups.js
@@ -2,36 +2,45 @@ import React from 'react';
 import { groupASample, groupBSample, groupCSample, edit } from '../../../FillerImages';
 import classes from './SocialGroups.module.scss';
 
-const { groupContainer, groupCell, groupImgContain, groupTitle } = classes;
+const { groupContainer, groupCell, groupImgContain, groupTitle, actionContainer } = classes;
 
 const socialGroups = (props) => {
     return (
         <section className={groupContainer}>
-            <div className={groupCell}>
+            <div className={groupCell} style={{ position: "relative" }}>
                 <div className={groupImgContain}>
+                    <div className={actionContainer}>
+                        <button>
+                            <img src={edit} />
+                        </button>
+                    </div>
                     <img src={groupASample} alt="example group" />
-                    <button>
-                        <img src={edit} />
-                    </button>
+
                     <h4 className={groupTitle}>Title</h4>
                 </div>
             </div>
             <div className={groupCell}>
                 <div className={groupImgContain}>
+                    <div className={actionContainer}>
+                        <button>
+                            <img src={edit} />
+                        </button>
+                    </div>
                     <img src={groupBSample} alt="example group" />
-                    <button>
-                        <img src={edit} />
-                    </button>
+           
                     <h4 className={groupTitle}>Title</h4>
 
                 </div>
             </div>
             <div className={groupCell}>
-                <div className={groupImgContain}>
-                    <img src={groupCSample} alt="example group" />
+                <div className={actionContainer}>
                     <button>
                         <img src={edit} />
                     </button>
+                </div>
+                <div className={groupImgContain}>
+                    <img src={groupCSample} alt="example group" />
+
                     <h4 className={groupTitle}>Title</h4>
                 </div>
 
