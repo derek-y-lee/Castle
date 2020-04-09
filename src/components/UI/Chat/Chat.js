9645622dc6a533b56964acfd74bf69afac543735
Derek <hdvvlee@gmail.com>
2020-04-08 21:41:21 -0700

    added youtube link

--- src/components/UI/Chat/Chat.js
+++ src/components/UI/Chat/Chat.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import classes from './Chat.module.scss';
+import { Link } from 'react-router-dom';
 import { youTube, netFlix } from '../../../FillerImages';
 
 const { chatWindow, Roulette, chatInput, send } = classes;
@@ -8,12 +9,12 @@ const chat = (props) => {
     return (
         <>
             <section className={chatWindow}>
-                <p>Chat window here</p>
+                <p>Chat here</p>
             </section>
             <section className={Roulette}>
-              <Link to = "www.youtube.com">
-                <img src={youTube} alt="YouTube icon" />
-              </Link>
+              <a href="https://www.youtube.com/">
+                <img src={youTube} alt="YouTube icon"/>
+              </a>
                 <img src={netFlix} alt="Netflix Icon"/>
                 <div>Roulette</div>
                 <div>Roulette</div>
