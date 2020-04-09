f51708522a6a40de0608259f4b564640ced67ad1
Wesley Webster <wesleywebster91@gmail.com>
2020-04-09 00:32:36 -0500

    Bug fixed friends

--- src/components/UI/Chat/Chat.js
+++ src/components/UI/Chat/Chat.js
@@ -1,14 +1,14 @@
-import { React, useState } from 'react';
+import React, { useState } from 'react';
 import classes from './Chat.module.scss';
 import { youTube, netFlix } from '../../../FillerImages';
 
 const { chatWindow, Roulette, chatInput, send } = classes;
 
 const Chat = (props) => {
-
-
-    const [YouTube, toOpenYouTube] = useState(false);
-    const toggle = () => toOpenYouTube(!YouTube);
+   
+    
+const [ YouTube, toOpenYouTube ] = useState(false);
+const toggle = () => toOpenYouTube(!YouTube);
 
     return (
         <>
@@ -16,8 +16,8 @@ const Chat = (props) => {
                 <p>Chat window here</p>
             </section>
             <section className={Roulette}>
-                <img src={youTube} alt="YouTube icon" onClick={toggle} />
-                <img src={netFlix} alt="Netflix Icon" />
+                <img src={youTube} alt="YouTube icon" />
+                <img src={netFlix} alt="Netflix Icon"/>
                 <div>Roulette</div>
                 <div>Roulette</div>
             </section>
