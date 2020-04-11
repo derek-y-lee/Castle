4174a31c86aea2f2f04fb382dfbbe1dcb2244745
Derek <hdvvlee@gmail.com>
2020-04-11 13:34:27 -0700

    no security issues

---
+++ config.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+
+const { Pool } = require('pg')
+const isProduction = process.env.NODE_ENV === 'production'
+NODE_TLS_REJECT_UNAUTHORIZED=0
+
+// const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+//
+// const pool = new Pool({
+//   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+//   ssl: isProduction,
+// })
+
+
+const constring = process.env.URI
+
+const pool = new Pool({
+  connectionString : constring,
+  ssl:true
+})
+pool.connect();
+
+module.exports = { pool }
