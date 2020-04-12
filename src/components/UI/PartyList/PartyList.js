2514ec5ce6744c92f1a7a8f3a8114cf538330c72
Wesley Webster <wesleywebster91@gmail.com>
2020-04-12 01:15:20 -0500

    Alternative way
    Key value pairs added

--- src/components/UI/PartyList/PartyList.js
+++ src/components/UI/PartyList/PartyList.js
@@ -8,12 +8,43 @@ import { FaceA, FaceB, FaceC, FaceD, FaceE, FaceF, FaceG, memberList } from '../
 const { partyContainer } = classes;
 
 
-const partyMemberList = 
+const partyMemberList = [
     {
         name: 'Mike Wakowski',
         rank: 'leader',
         image: <img src={FaceA} alt="" />
+    },
+    {
+        name: 'Sarah Lee',
+        rank: 'member',
+        image: <img src={FaceB} alt="" />
+    },
+    {
+        name: 'Michael Chu',
+        rank: 'member',
+        image: <img src={FaceC} alt="" />
     }
+    {
+        name: 'Gina Tran',
+        rank: 'member',
+        image: <img src={FaceD} alt="" />
+    }
+    {
+        name: 'Laurin Hill',
+        rank: 'member',
+        image: <img src={FaceE} alt="" />
+    }
+    {
+        name: 'Wade Wilson',
+        rank: 'member',
+        image: <img src={FaceF} alt="" />
+    },
+    {
+        name: 'Gwen Stacey',
+        rank: 'member',
+        image: <img src={FaceG} alt="" />
+    }
+]
 
 
 // Would need to turn object into an array and loop through an array. Items inside array would be user data.
